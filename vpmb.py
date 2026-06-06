681689c1986c8c67a93ebda1e2d7907b2adf53b6
Roman Markovtsev <sns@rvm.cc>
2026-06-06 21:25:05 +0900

    Add vpmb.py VPM-B Python engine

---
+++ vpmb.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Copyright 2010, Bryan Waite, Erik C. Baker. All rights reserved.
+# VPM-B Python reference engine for LSP D-Planner
+# See full implementation in repository
+
+# This file is the VPM-B decompression algorithm Python reference implementation.
+# Full source: https://github.com/Three-Cats-LSP/LSP_D-planner
